475405af8f3f4b6867390743ab2b5c625644b902
b0x3n <jorge.gomzar@gmail.com>
2018-12-11 12:44:08 +0100

    improved code

--- macListRegister.py
+++ macListRegister.py
@@ -1,13 +1,16 @@
 import os
 from sys import platform
+from xml.dom import minidom
 
 def clear():
+	""" Clear function multi os """
 	if platform == "linux":
 		_ = os.system('clear')
 	elif platform == "win32":
 		_ = os.system('cls')
 
 def macizer(mac):
+	""" Formats input to look like a MAC address """
 	newMac = []
 	count = 0
 	vecCount = [2, 4, 6, 8, 10]
@@ -18,30 +21,41 @@ def macizer(mac):
 		count = count + 1
 	return ''.join(newMac)
 
-def write2file(f, id, mac):
-	cadena = ' [+] ID: ' + id + ' | MAC: ' + mac + '\n'
-	f.write(cadena)
-	print(cadena)
+def prettierString(id, mac):
+	""" Converts the input into a prettier string """
+	return ' [+] ID: {} | MAC: {}\n'.format(id, mac)
 
 useIds = False
+dictMAC = {}
+
 if(input('¿Id? (s: sí): ').lower() == 's'):
 	useIds = True
 
-folder = str(input('Nombre de la red: '))
-os.mkdir(folder)
+filename = str(input('Nombre de la red: '))
 
-file = open('./'+folder+'/listadoMac.txt', 'w')
 if not useIds:
 	id_device = 1
 else:
 	id_device = input('ID: ')
-macAddr = macizer(input('MAC: '))
+# macAddr = macizer(input('MAC: '))
+macAddr = input('MAC: ')
+
 while  macAddr != '':
 	clear()
-	write2file(file, str(id_device), macAddr)
+	dictMAC[id_device] = macAddr
+	print(prettierString(id_device, macAddr))
+
 	if not useIds:
 		id_device = id_device + 1
 	else:
 		id_device = input('ID: ')
-	macAddr = macizer(input('MAC: '))	
-file.close()
+	# macAddr = macizer(input('MAC: '))
+	macAddr = input('MAC: ')
+
+with open('./{}.txt'.format(filename), 'w') as file:
+	for k, v in dictMAC.items():
+		file.write(prettierString(k, v))
+
+# with open('./{}.xml'.format(filename), 'w') as file:
+	# XML IMPLEMENTATION
+
